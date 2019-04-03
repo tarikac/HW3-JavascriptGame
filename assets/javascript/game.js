427daaedfcad30894e9a4ed86d589163988687cc
tarikac <tarika.common@gmail.com>
2019-04-02 19:17:13 -0500

    i believe it is working close to how its supposed to work. I included an alert for one someone wins a point

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 var wins = 0;
 var losses = 0;
 var presses = 9;
-var test = "";
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guesses = [];
 
@@ -15,7 +14,9 @@ var storeLoss = document.getElementById("number-loss");
 var attempts = document.getElementById("guess-left"); 
 var tries = document.getElementById("letters-guessed");
 
-//reference back to originally rps game
+
+// need to send presses to display in the guess-left span
+//attempts.textContent = presses;
 
 document.onkeyup = function(event) {
     //may need to add this to a for loop? 
@@ -28,17 +29,22 @@ document.onkeyup = function(event) {
     // tries.appendChild=(event.key);
     
     // was trying to figure out how best to treat the decrease
-    if(event.key=true){
+    if(event.key = true){
         presses--;
-    
+        attempts.textContent = presses;
     }
+
+   
+
+    // if(guesses.length > 8 || event.key == randomLetters){
+    //         guesses = [];
+    // }
 
     //want to make sure once it is set to 0, it resets
     //not clean and needs to simplified better
-    if(presses==0){
-        presses = 0;
-        attempts.textContent = presses;
-        presses = 10;
+    if(presses<1){
+       // attempts.textContent = presses;
+        presses = 9;
         randomLetters = letters[Math.floor(Math.random() * letters.length)];
         losses++;
         guesses = []; //looks like this resets the array!
@@ -48,12 +54,17 @@ document.onkeyup = function(event) {
 //do this if key pressed matches the number from randomly generated
 //this also needs to reset the counter when pressed switched from forloop to just using ifs
     else if (event.key == randomLetters) {
+        randomLetters = letters[Math.floor(Math.random() * letters.length)];
+        guesses = [];
+        presses = 9;
+        attempts.textContent = presses;
         wins++;
+        alert("You won a point!");
     }
     //sending results back to the html page
     storeWins.textContent = wins;
     storeLoss.textContent = losses;
-    attempts.textContent = presses;//i have this in the if statement
+    attempts.textContent = presses;
 
         
         
